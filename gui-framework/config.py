0861faa074011accc29ba058a59100a49c1ba82d
Yilong94 <yilong_tan@mymail.sutd.edu.sg>
2019-06-24 11:06:08 +0800

    Updated config file

--- gui-framework/config.py
+++ gui-framework/config.py
@@ -4,21 +4,24 @@
 model = {
     'normal':'normal/expt_2019-04-28_19-18-34/rnnmodel_2019-04-28_19-18-34.h5',
     'thc-tls-dos':'thc-tls-dos/expt_2019-04-30_07-31-14/rnnmodel_2019-04-30_07-31-14.h5',
-    # 'sample':'sample/expt_2019-03-17_12-20-30/rnnmodel_2019-03-17_12-20-30.h5'
 }
 
 # given path: feature_extraction/extracted-features/
 features = {
     'normal':'normal/',
     'thc-tls-dos':'thc-tls-dos/',
-    # 'sample':'sample/'
+    # 'sample':'sample/',
+    'malware':'malware/',
+    'poodle':'poodle/',
 }
 
 # given path: path/to/root/dir/where/pcap/files/are/stored/
 pcapfiles = {
     'normal':'normal/',
     'thc-tls-dos':'thc-tls-dos/',
-    # 'sample':'sample/'
+    # 'sample':'sample/',
+    'malware':'malware/',
+    'poodle':'poodle/',
 }
 # note: the directory paths must be exact. For instance, the directory to pcap files for normal/ and thc-tls-dos/ must be found in the same parent directory
 
@@ -32,7 +35,19 @@ results = {
         'thc-tls-dos':{
             'train':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_thc-tls-dos/train/results.csv',
             'val':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_thc-tls-dos/val/results.csv'
-        }
+        },
+        'malware':{
+            'train':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_malware/train/results.csv',
+            'val':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_malware/val/results.csv'
+        },
+        'poodle':{
+            'train':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_poodle/train/results.csv',
+            'val':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_poodle/val/results.csv'
+        },
+        # 'sample':{
+        #     'train':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_sample/train/results.csv',
+        #     'val':'normal/expt_2019-04-28_19-18-34/predict_results/predict_on_sample/val/results.csv'
+        # },
     },
     'thc-tls-dos':{
         'normal':{
@@ -42,14 +57,16 @@ results = {
         'thc-tls-dos':{
             'train':'thc-tls-dos/expt_2019-04-30_07-31-14/predict_results/predict_on_thc-tls-dos/train/results.csv',
             'val':'thc-tls-dos/expt_2019-04-30_07-31-14/predict_results/predict_on_thc-tls-dos/val/results.csv'
-        }
+        },
+        'malware':{
+            'train':'thc-tls-dos/expt_2019-04-30_07-31-14/predict_results/predict_on_malware/train/results.csv',
+            'val':'thc-tls-dos/expt_2019-04-28_19-18-34/predict_results/predict_on_malware/val/results.csv'
+        },
+        'poodle':{
+            'train':'thc-tls-dos/expt_2019-04-30_07-31-14/predict_results/predict_on_poodle/train/results.csv',
+            'val':'thc-tls-dos/expt_2019-04-30_07-31-14/predict_results/predict_on_poodle/val/results.csv'
+        },
     },
-    # 'sample':{
-    #     'sample':{
-    #         'train':'sample/expt_2019-03-17_12-20-30/predict_results/predict_on_sample/train/results.csv',
-    #         'val':'sample/expt_2019-03-17_12-20-30/predict_results/predict_on_sample/val/results.csv'
-    #     }
-    # }
 }
 
 criteria = {
