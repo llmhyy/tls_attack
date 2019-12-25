02a03b47754d08f7680edef019148c305ca3f8fd
Junyang <ju-nyang@live.com>
2019-12-25 16:41:23 +0800

    reverted back to using full sample for hypothesis test

--- significance-test/hypothesis-testing.py
+++ significance-test/hypothesis-testing.py
@@ -27,12 +27,10 @@ args = parser.parse_args()
 # Initializing model directory for prediction against other traffic
 model_dir = args.model
 model = model_dir.split("\\")[-1].split("-")[0]
-print(model)
 predict_dir = os.path.join(model_dir, os.listdir(model_dir)[0], "predict_results")
 
 for folder in os.listdir(predict_dir):
 	dataset = folder.split("_")[-1]
-	print(dataset)
 	results_dir = os.path.join(predict_dir, folder, "val")
 	if(dataset == model):
 		model_result = get_acc(results_dir, model)
@@ -51,12 +49,9 @@ for folder in os.listdir(predict_dir):
 	# p-value < 0.05 : Reject null hypothesis || else, DO NOT reject null hypothesis
 	###################################################################################
 	if all(v is not None for v in [model_result, test_result]):
-		# Performing random sampling on the population dataset
-		model_sample = random.sample(model_result, k=100)
-		test_sample = random.sample(test_result, k=100)
 		# Plotting graph to show distribution of sampled data
-		plot_graph(test_sample, dataset, model)
+		plot_graph(test_result, dataset, model)
 
 		print("Testing " + str(dataset) + " on " + model)
-		statistic, pVal = stats.mannwhitneyu(model_sample, test_sample)
+		statistic, pVal = stats.mannwhitneyu(model_result, test_result)
 		print ('P value: ', pVal)
